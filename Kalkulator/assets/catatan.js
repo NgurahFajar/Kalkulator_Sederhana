c75475a254f7de2c860be05d7a920a99be5d4eac
I Gusti Agung Ngurah Fajar Dharmawangsa <96526566+NgurahFajar@users.noreply.github.com>
2022-05-03 21:12:21 +0700

    Add files via upload

---
+++ Kalkulator/assets/catatan.js
@@ -0,0 +1,30 @@
+// lebih baik menggunakan let menggantikan var
+// untuk immutable menggunakan const
+
+// === itu berarti identik, biasanya buat ngecek tipe data mesti wajib sama
+
+// || merupakan or
+
+/*
+    For of loop
+    syntax :
+    for(arrayItem of myArray) {
+    // do something
+
+    contoh :
+    let myArray = ["Harry", "Ron", "Hermione", "Tom"];
+
+for(const arrayItem of myArray) {
+    console.log(arrayItem)
+}
+
+output
+Harry
+Ron
+Hermione
+Tom
+
+}
+
+
+*/
